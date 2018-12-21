ad0906b1ab3d9eed2c586a9c652b44bdcaccaa86
Traysi <link@traysi.org>
2018-12-21 16:51:51 -0600

    s/raven/rito/g/

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
 
-var ravencore = require('ravencore-lib');
+var ritocore = require('ritocore-lib');
 
-module.exports = ravencore;
+module.exports = ritocore;
--- test/index.js
+++ test/index.js
@@ -1,27 +1,27 @@
 'use strict';
 
 var should = require('chai').should();
-var ravencore = require('../');
+var ritocore = require('../');
 
 describe('Library', function() {
   it('should export primatives', function() {
-    should.exist(ravencore.crypto);
-    should.exist(ravencore.encoding);
-    should.exist(ravencore.util);
-    should.exist(ravencore.errors);
-    should.exist(ravencore.Address);
-    should.exist(ravencore.Block);
-    should.exist(ravencore.MerkleBlock);
-    should.exist(ravencore.BlockHeader);
-    should.exist(ravencore.HDPrivateKey);
-    should.exist(ravencore.HDPublicKey);
-    should.exist(ravencore.Networks);
-    should.exist(ravencore.Opcode);
-    should.exist(ravencore.PrivateKey);
-    should.exist(ravencore.PublicKey);
-    should.exist(ravencore.Script);
-    should.exist(ravencore.Transaction);
-    should.exist(ravencore.URI);
-    should.exist(ravencore.Unit);
+    should.exist(ritocore.crypto);
+    should.exist(ritocore.encoding);
+    should.exist(ritocore.util);
+    should.exist(ritocore.errors);
+    should.exist(ritocore.Address);
+    should.exist(ritocore.Block);
+    should.exist(ritocore.MerkleBlock);
+    should.exist(ritocore.BlockHeader);
+    should.exist(ritocore.HDPrivateKey);
+    should.exist(ritocore.HDPublicKey);
+    should.exist(ritocore.Networks);
+    should.exist(ritocore.Opcode);
+    should.exist(ritocore.PrivateKey);
+    should.exist(ritocore.PublicKey);
+    should.exist(ritocore.Script);
+    should.exist(ritocore.Transaction);
+    should.exist(ritocore.URI);
+    should.exist(ritocore.Unit);
   });
 });
